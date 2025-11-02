12cf905a892d7b4a6056bc0afff1baefac98e846
AendriSingh <aendri4@gmail.com>
2025-11-01 19:06:45 -0500

    Comments for Message/MessageManager
    Comments for Message and MessageManager, encryption setup in Message Manager

--- Message.java
+++ Message.java
@@ -1,12 +1,14 @@
 /* CS6349 Network Security: Secure Relay-Based Chat System
  * By Parisa Nawar (pxn210032) and Aendri Singh (axs210369)
  * 
- * Message Class: A message object
+ * Message Class: Stores information on a message 
  */
 
 import java.time.LocalDateTime;
 
-// Signifies the type of message and determines how it will be read/interpreted by the system. 
+/**
+ * Signifies the type of message and determines how it will be read/interpreted by the system.
+ */
 enum Opcode {
     REGI, // Registration
     SESR, // Establish Session Key with Relay
@@ -19,8 +21,12 @@ enum Opcode {
     ERRR  // Error: Registration Invalid
 }
 
+/**
+ * The Message class stores header and body information on a message.
+ */
 public class Message {
     private final int NUM_REQUIRED_HEADERS = 5;
+    private final String HEADER_SEPERATOR = " - ";
     // HEADERS:
     private Opcode opcode;
     private String sender;
@@ -30,7 +36,12 @@ public class Message {
     private int sessionID = -1;
     private String body;
 
-    // Create message with NO body
+    /**
+     * Creates a message object with no body.
+     * @param opcode Message type
+     * @param sender The original sender of the message 
+     * @param receiver The final receipient of the message
+     */
     public Message (Opcode opcode, String sender, String receiver) {
         this.opcode = opcode;
         this.sender = sender;
@@ -39,7 +50,13 @@ public class Message {
         timestamp = LocalDateTime.now();
     }
 
-    // Create message WITH body
+    /**
+     * Creates a message object with a body.
+     * @param opcode Message type
+     * @param sender The original sender of the message 
+     * @param receiver The final receipient of the message
+     * @param body The message's contents
+     */
     public Message (Opcode opcode, String sender, String receiver, String body) {
         this.opcode = opcode;
         this.sender = sender;
@@ -50,9 +67,13 @@ public class Message {
         size = body.length();
     }
 
-    // Decode and create message
+    /**
+     * Decodes a message string and creates a message object.
+     * @param message The message in string format
+     * @throws Exception Indicates an invalid message format
+     */
     public Message (String message) throws Exception{
-        String[] headers = message.split(" - ", NUM_REQUIRED_HEADERS + 1);
+        String[] headers = message.split(HEADER_SEPERATOR, NUM_REQUIRED_HEADERS + 1);
 
         // Verify number of headers
         if (headers.length < NUM_REQUIRED_HEADERS) { throw new InvalidMessageFormat(); }
@@ -61,65 +82,83 @@ public class Message {
         for (int i = 0; i < NUM_REQUIRED_HEADERS; i++) {
             String[] parts = headers[i].split(": ", 2);
             
+            // OPCODE
             if (parts[0].equals("Opcode")) {
                 opcode = Opcode.valueOf(parts[1]);
             }
+            // SENDER
             else if (parts[0].equals("Sender")) {
                 sender = parts[1];
             } 
+            // RECEIVER
             else if (parts[0].equals("Receiver")) {
                 receiver = parts[1];
             } 
+            // TIMESTAMP
             else if (parts[0].equals("Timestamp")) {
                 timestamp = LocalDateTime.parse(parts[1]);
             } 
+            // SIZE
             else if (parts[0].equals("Size")) {
                 size = Integer.parseInt(parts[1]);
             }
+            // Unknown header
             else {
                 throw new InvalidMessageFormat();
             }
         }
 
-        // Parse optional headers
+        // OPTIONAL HEADERS:
         if (headers.length > NUM_REQUIRED_HEADERS) {
-            // session id
+            // SESSION ID
             int headerPos = headers[NUM_REQUIRED_HEADERS].indexOf(": ");
             if (headerPos > 0) {
                 String header = headers[NUM_REQUIRED_HEADERS].substring(0, headerPos);
                 if (header.equals("SessionID")) {
-                    sessionID = Integer.parseInt(header.substring(0, header.indexOf("\n")));
+                    // If end of session ID found
+                    if (header.indexOf(HEADER_SEPERATOR) > 0) {
+                        sessionID = Integer.parseInt(header.substring(0, header.indexOf(HEADER_SEPERATOR)));
+                    } 
+                    else { throw new InvalidMessageFormat("Missing SessionID end.");  }   
                 }
-            }
-
-            // body
-            if (size > 0) {
-                headerPos = headers[NUM_REQUIRED_HEADERS].indexOf("Body: ");
-                body = headers[NUM_REQUIRED_HEADERS].substring(headerPos + 6);
             } 
         } 
-        // Body header missing!
-        else if (size > 0) {
-            throw new InvalidMessageFormat();
+        // BODY
+        if (size > 0) {
+            // If optional header exists
+            if (headers.length > NUM_REQUIRED_HEADERS) {
+                int headerPos = headers[NUM_REQUIRED_HEADERS].indexOf("Body: ");
+                // If body header is found
+                if (headerPos > 0) {
+                    body = headers[NUM_REQUIRED_HEADERS].substring(headerPos + 6);
+                }
+                else { throw new InvalidMessageFormat("Missing Body header.");  }
+            } else { throw new InvalidMessageFormat("Missing Body header.");  }
+               
         }
     }
 
-    // Sets the sessionID
+    /**
+     * Sets the sessionID to the given values.
+     * @param sessionID
+     */
     public void setSessionID (int sessionID) {
         this.sessionID = sessionID;
     }
 
-    // Returns the message in string format
+    /**
+     * Returns the message object in string format.
+     */
     public String toString() {
         String message = 
-            "Opcode: " + opcode.name() + " - " +
-            "Sender: " + sender + " - " +
-            "Receiver: " + receiver + " - " +
-            "Timestamp: " + timestamp + " - " +
-            "Size: " + size + " - ";
+            "Opcode: " + opcode.name() + HEADER_SEPERATOR +
+            "Sender: " + sender + HEADER_SEPERATOR +
+            "Receiver: " + receiver + HEADER_SEPERATOR +
+            "Timestamp: " + timestamp + HEADER_SEPERATOR +
+            "Size: " + size + HEADER_SEPERATOR;
 
             if (sessionID > 0) {
-                message += "SessionID: " + sessionID + " - ";
+                message += "SessionID: " + sessionID + HEADER_SEPERATOR;
             }
 
             if (size > 0) {
@@ -129,20 +168,65 @@ public class Message {
         return message;
     }
 
+    /**
+     * Returns the opcode of the message. 
+     * @return Opcode
+     */
     public Opcode getOpcode() { return opcode; }
+
+    /**
+     * Returns the original sender of the message.
+     * @return Sender
+     */
     public String getSender() { return sender; }
+
+    /**
+     * Returns the final receipient of the message.
+     * @return Receiver
+     */
     public String getReceiver() { return receiver; }
+
+    /**
+     * Returns the timestamp of the message.
+     * @return Timestamp
+     */
     public LocalDateTime getTimestamp() { return timestamp; }
+
+    /**
+     * Returns the size of the message body.
+     * @return Size
+     */
     public int getSize() { return size; }
+
+    /**
+     * Returns the SessionID of the message. 
+     * -1 if sessionID doesn't exist.
+     * @return SessionID
+     */
     public int getSessionID() { return sessionID; }
+
+    /**
+     * Returns the message body of the message.
+     * @return Body
+     */
     public String getBody() { return body; }
 }
 
-// Exception for invalid message formats
+/**
+ * Exception if invalid message formats.
+ */
 class InvalidMessageFormat extends Exception {
+    /**
+     * Invalid message format.
+     */
     public InvalidMessageFormat() {
         super();
     }
+
+    /**
+     * Invalid message format.
+     * @param m Display message
+     */
     public InvalidMessageFormat(String m) {
         super(m);
     }
--- MessageManager.java
+++ MessageManager.java
@@ -6,102 +6,268 @@
 
 import java.io.*;
 import java.util.concurrent.*;
-
 import javax.crypto.SecretKey;
-
 import java.net.*;
 import java.security.PrivateKey;
 import java.security.PublicKey;
 import java.time.LocalDateTime;
 
+/**
+ * MessageManager encodes and decodes messages between 2 endpoints.
+ */
 public class MessageManager {
+    private final int SESSION_DURATION_MINS = 15;
+
     private String source;
     private String destination;
    
-    private PrivateKey srcKey;
-    private PublicKey destKey;
+    private PrivateKey srcPrivKey;
+    private PublicKey destPubKey;
 
-    private boolean sessionMode = false;
+    private boolean sessionMode = false; //If a session is active
     private SecretKey sessionKey;
     private int sessionID;
     private LocalDateTime sessionExpiretime;
 
-    // No public Key
-    public MessageManager(String source, PrivateKey srcKey, String destination) 
+    /**
+     * Creates a MessageManager with an unknown destination public key.
+     * @param source Source's name
+     * @param srcPrivKey Source's private key 
+     * @param destination Destination's name
+     * @throws FileNotFoundException Destination's public key could not be found.
+     */
+    public MessageManager(String source, PrivateKey srcPrivKey, String destination) 
         throws FileNotFoundException
     {
         this.source =  source;
         this.destination =  destination;
 
         KeyHandler keyhand = new KeyHandler();
-        this.srcKey = srcKey;
-        destKey = keyhand.getUserPublicKey(destination);
+        this.srcPrivKey = srcPrivKey;
+        destPubKey = keyhand.getUserPublicKey(destination);
         
         // Check if destination public key was found
-        if (destKey == null) {
-            throw new FileNotFoundException();
+        if (destPubKey == null) {
+            throw new FileNotFoundException("Destination's public key could not be found.");
         }
     }
 
-    // With public key
-    public MessageManager(String source, PrivateKey srcKey, String destination, PublicKey destKey) 
-        throws FileNotFoundException
-    {
+    /**
+     * Creates a MessageManager.
+     * @param source Source's name
+     * @param srcPrivKey Source's private key 
+     * @param destination Destination's name
+     * @param destPubKey Destination's public key
+     */
+    public MessageManager(String source, PrivateKey srcPrivKey, String destination, PublicKey destPubKey) {
         this.source =  source;
         this.destination =  destination;
-
-        this.srcKey = srcKey;
-        this.destKey = destKey;
+        this.srcPrivKey = srcPrivKey;
+        this.destPubKey = destPubKey;
     }
     
-    // Sets the session key, sessionID, and activates session mode for messages
-    public void setSession(SecretKey sessionKey, int sessionID) {
+    /**
+     * Activates Session Mode and sets the relevant fields. 
+     * @param sessionKey Session key
+     * @param sessionID Identifier for current session 
+     * @param expiry The expiry time for the session
+     */
+    public void setSession(SecretKey sessionKey, int sessionID, LocalDateTime expiry) {
+        sessionMode = true;
         this.sessionKey = sessionKey;
         this.sessionID = sessionID;
-        sessionMode = true;
+        this.sessionExpiretime = expiry;
     }
 
-    // Resets the session key by deactivating session mode
+    /**
+     * Activates Session Mode and sets the relevant fields. Calculates 
+     * and returns session expiry time.
+     * @param sessionKey Session key
+     * @param sessionID Identifier for current session
+     */
+    public LocalDateTime setSession(SecretKey sessionKey, int sessionID) {
+        sessionMode = true;
+        this.sessionKey = sessionKey;
+        this.sessionID = sessionID;
+        this.sessionExpiretime = LocalDateTime.now().plusMinutes(SESSION_DURATION_MINS);
+
+        return sessionExpiretime;
+    }
+
+    /**
+     * Resets the session key by deactivating session mode
+     */
     public void resetSession() {
         sessionMode = false;
     }
 
-    // Creates an encrypted message NO body
+    /**
+     * Creates an encrypted message with no body.
+     * @param opcode Message type
+     * @return Encrypted message
+     */
     public String encodeMessage(Opcode opcode) {
         Message message = new Message(opcode, source, destination);
         if (sessionMode) {message.setSessionID(sessionID); }
-        return message.toString(); // *** NEED TO ENCRYPT
+
+        // Encrypt + Hash
+        if (sessionMode) { return encryptHashSession(message.toString()); } 
+        else { return encryptHashRSA(message.toString()); }
     }
 
-    // Creates an encrypted message WITH body
+    /**
+     * Creates an encrypted message with a body.
+     * @param opcode Message type
+     * @param body Message body
+     * @return Encrypted message
+     */
     public String encodeMessage(Opcode opcode, String body) {
         Message message = new Message(opcode, source, destination, body);
         if (sessionMode) {message.setSessionID(sessionID); }
-        return message.toString(); // *** NEED TO ENCRYPT
+        
+        // Encrypt + Hash
+        if (sessionMode) { return encryptHashSession(message.toString()); } 
+        else { return encryptHashRSA(message.toString()); }
     }
 
-    // Creates an encrypted message NO body and different sender/receiver
+    /**
+     * Creates an encrypted message with no body and different sender/receiver.
+     * @param opcode Message type
+     * @param sender Original source
+     * @param receiver Final receipient
+     * @return Encrypted message
+     */
     public String encodeMessage(Opcode opcode, String sender, String receiver) {
         Message message = new Message(opcode, sender, receiver);
         if (sessionMode) {message.setSessionID(sessionID); }
-        return message.toString(); // *** NEED TO ENCRYPT
+        
+        // Encrypt + Hash
+        if (sessionMode) { return encryptHashSession(message.toString()); } 
+        else { return encryptHashRSA(message.toString()); }
     }
 
-    // Creates an encrypted message WITH body and different sender/receiver
+    /**
+     * Creates an encrypted message with a body and different sender/receiver.
+     * @param opcode Message type
+     * @param sender Original source
+     * @param receiver Final receipient
+     * @param body Message body
+     * @return Encrypted message
+     */
     public String encodeMessage(Opcode opcode, String sender, String receiver, String body) {
         Message message = new Message(opcode, sender, receiver, body);
         if (sessionMode) {message.setSessionID(sessionID); }
-        return message.toString(); // *** NEED TO ENCRYPT
+        
+        // Encrypt + Hash
+        if (sessionMode) { return encryptHashSession(message.toString()); } 
+        else { return encryptHashRSA(message.toString()); }
     }
 
-    // Parses the given message and returns a message object
+    //%%% INCOMPLETE %%%//
+    /**
+     * Parses and decodes the given message and returns the message object
+     * @param data
+     * @return Message object 
+     * @throws Exception Invalid message
+     */
     public Message decodeMessage(String data) throws Exception{
-        // *** NEED TO DECRYPT
-        Message message = new Message(data);
+        String decryptData;
+        if (sessionMode) {
+            decryptData = decryptHashSession(data);
+        }
+        else {
+            decryptData = decryptHashRSA(data);
+        }
+
+        Message message = new Message(decryptData);
         return message;
     }
 
+    //%%% INCOMPLETE %%%//
+    /**
+     * Encrypts with RSA and adds a digital signature to the given plaintext.
+     * @param plaintext Plaintext
+     * @return Ciphertext
+     */
+    private String encryptHashRSA (String plaintext) {
+        String ciphertext = plaintext;
+        // Encryption: RSA (dest's public key)
+        // Key-Hash: Digital Signature (sender's private key)
+        return ciphertext;
+    }
 
+    //%%% INCOMPLETE %%%//
+    /**
+     * Encrypts with shared secret key and adds an HMAC to the given plaintext.
+     * @param plaintext Plaintext
+     * @return Ciphertext
+     */
+    private String encryptHashSession (String plaintext) {
+        String ciphertext = plaintext;
+        // Encryption: Secret Key (session key)
+        // Key-Hash: HMAC (session key)
+        return ciphertext;
+    }
+
+    //%%% INCOMPLETE %%%//
+    /**
+     * Decrypts the given plaintext with RSA and verifies the integrity of the 
+     * message.
+     * @param ciphertext Ciphertext
+     * @return Plaintext
+     * @throws CannotVerifyIntegrity Message contents have been altered
+     */
+    private String decryptHashRSA (String ciphertext) throws CannotVerifyIntegrity {
+        String plaintext = ciphertext;
+        // Decryption: RSA (dest's public key)
+        // Key-Hash: Digital Signature (sender's private key)
+
+        // Verify integrity
+        boolean validKeyedHash = false;
+        if (!validKeyedHash) { throw new CannotVerifyIntegrity(); }
+
+        return plaintext;
+    }
+
+    //%%% INCOMPLETE %%%//
+    /**
+     * Decrypts the given plaintext with a shared secret key and verifies the 
+     * integrity of the message.
+     * @param ciphertext
+     * @return
+     * @throws CannotVerifyIntegrity
+     */
+    private String decryptHashSession (String ciphertext) throws CannotVerifyIntegrity {
+        String plaintext = ciphertext;
+        // Decryption: Secret Key (session key)
+        // Key-Hash: HMAC (session key)
+
+        // Verify integrity
+        boolean validKeyedHash = false;
+        if (!validKeyedHash) { throw new CannotVerifyIntegrity(); }
+
+        return plaintext;
+    }
+}
+
+/**
+ * Exception if invalid hash.
+ */
+class CannotVerifyIntegrity extends Exception {
+    /**
+     * Exception if invalid hash.
+     */
+    public CannotVerifyIntegrity() {
+        super();
+    }
+
+    /**
+     * Exception if invalid hash.
+     * @param m Display message
+     */
+    public CannotVerifyIntegrity(String m) {
+        super(m);
+    }
 }
 
 
